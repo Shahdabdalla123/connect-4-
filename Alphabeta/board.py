a423b5564b4d655eee1d5bf67873535cb6254a62
shereen-shawky <116681637+shereen-shawky@users.noreply.github.com>
2023-05-17 21:03:33 +0300

    Update board.py
    scoring overall attractiveness of a board after a piece has been dropped and implementing alpha beta pruning algorithm

--- Alphabeta/board.py
+++ Alphabeta/board.py
@@ -40,6 +40,120 @@ def make_Gui_Board(board):
 
     pygame.display.update()
 
+def Calculate_score_position(board, piece):
 
+    Point = 0
+    center_array = [int(i) for i in list(board[:, Colum_Num // 2])] #Colum in center
+    center_count = center_array.count(piece)
+    Point += center_count * 6
+
+    for x in range(Row_Num):
+        row_array = [int(i) for i in list(board[x, :])]
+        for y in range(Colum_Num - 3):
+            window = row_array[y:y + 4]
+            Point += calculate_window(window, piece)
+
+    # score vertical
+    for x in range(Colum_Num):
+        col_array = [int(i) for i in list(board[:, x])]
+        for y in range(Row_Num - 3):
+            window = col_array[y:y + 4]
+            Point += calculate_window(window, piece)
+
+    # score positively sloped diagonals
+    for x in range(3, Row_Num):
+        for y in range(Colum_Num - 3):
+            window = [board[x - i][y + i] for i in range(4)]
+            Point += calculate_window(window, piece)
+
+    # score negatively sloped diagonals
+    for x in range(3, Row_Num):
+        for y in range(3, Colum_Num):
+            window = [board[x - i][y - i] for i in range(4)]
+            Point += calculate_window(window, piece)
+
+    return Point
+
+
+def Terminal_Test(board):
+    return Win(board, AI_PIECE) or Win(board, Computer_PIECE) or len(get_valid_Colums(board)) == 0
+
+
+def Alphabeta(board, depth, alpha, beta, MaxFlag):
+    # all valid Colums on the board
+    Valid_Colum = get_valid_Colums(board)
+    Terminal = Terminal_Test(board)
+
+
+    if depth == 0 or Terminal:
+        if Terminal:  # winning move
+            if Win(board, Computer_PIECE):
+                 return (None, -10000000)
+            elif Win(board, AI_PIECE):
+                return (None, 10000000)
+            else:
+                return (None, 0)
+
+        else:  # depth is zero
+            return (None, Calculate_score_position(board, AI_PIECE))
+
+
+    if MaxFlag:
+
+
+        value = -math.inf
+        column = random.choice(Valid_Colum)
+
+        for col in Valid_Colum:
+            row = Top_Row_Empty(board, col)
+            b_copy = board.copy()
+            put_piece(b_copy, row, col, AI_PIECE)
+            new_score = Alphabeta(b_copy, depth - 1, alpha, beta, False)[1]
+
+            if new_score > value:
+                value = new_score
+                column = col
+
+            alpha = max(value, alpha)
+
+            if alpha >= beta:
+                break
+
+        return column, value
+
+
+    else:  # for thte minimizing player
+        value = math.inf
+        column = random.choice(Valid_Colum)
+        for col in Valid_Colum:
+            row = Top_Row_Empty(board, col)
+            b_copy = board.copy()
+            put_piece(b_copy, row, col, Computer_PIECE)
+            new_score = Alphabeta(b_copy, depth - 1, alpha, beta, True)[1]
+            if new_score < value:
+                value = new_score
+                column = col
+            beta = min(value, beta)
+            if alpha >= beta:
+                break
+        return column, value
+
+
+# get all columns where a piece can be
+def get_valid_Colums(board):
+    valid_locations = []
+
+    for column in range(Colum_Num):
+        if Empty_Colum(board, column):
+            valid_locations.append(column)
+
+    return valid_locations
+
+
+
+def Game_Over():
+    global game_over
+    game_over = True
+    print(game_over)
 
 
